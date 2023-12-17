49059d84e08c2c86996731261443a3ed93f49885
dsl2022 <2015rpro@gmail.com>
2023-12-16 21:46:34 -0500

    updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.region # Replace with your desired region
-}
-
 resource "aws_s3_bucket" "web_bucket" {
   bucket = var.bucket_name # Replace with your desired bucket name
   tags = {
