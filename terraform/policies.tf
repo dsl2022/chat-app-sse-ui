256531ad78a1cad4fe80ef82e2c84480461c0ba3
dsl2022 <2015rpro@gmail.com>
2023-12-16 22:16:23 -0500

    updated terraform

--- terraform/policies.tf
+++ terraform/policies.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_policy" "web_bucket_policy" {
       {
         Effect    = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.oai.id}"
+          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.web_identity.id}"
         }
         Action   = "s3:*"
         Resource = "${aws_s3_bucket.web_bucket.arn}/*"
