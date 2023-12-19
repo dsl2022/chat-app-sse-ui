422d6154a5ba273a098da884ba11dde07cf238d9
dsl2022 <2015rpro@gmail.com>
2023-12-18 22:01:11 -0500

    added tailwind and refractor chat components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, ChangeEvent } from 'react';
-import './App.css';
-
+// import './App.css';
+import { Chat } from './components/Chat';
 // Define the structure of a message
 interface Message {
   user: string;
@@ -8,70 +8,80 @@ interface Message {
 }
 
 function App() {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [currentMessage, setCurrentMessage] = useState<string>("");
-  const [userInput, setUserInput] = useState<string>('');
+  const [lightMode, setLightMode] = useState<'dark' | 'light'>('dark');
+  // const [messages, setMessages] = useState<Message[]>([]);
+  // const [currentMessage, setCurrentMessage] = useState<string>("");
+  // const [userInput, setUserInput] = useState<string>('');
 
-  // Handle input change with proper event type
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserInput(e.target.value);
-  };
+  // // Handle input change with proper event type
+  // const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   setUserInput(e.target.value);
+  // };
 
-  const handleSendMessage = () => {
-    sendMessageToServer(userInput);
-    setUserInput(''); // Clear the input field
-  };
+  // const handleSendMessage = () => {
+  //   sendMessageToServer(userInput);
+  //   setUserInput(''); // Clear the input field
+  // };
 
-  const sendMessageToServer = async (message: string) => {
-    try {
-      setMessages(prevMessages => [...prevMessages, { user: 'You', text: message }]);
-      const response = await fetch('http://chat-app-load-balancer-381258406.us-east-1.elb.amazonaws.com/send-message', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message })
-      });
-      // const data = await response.json();
-      // Code to handle AI's response (currently commented out)
-      // setMessages(prevMessages => [...prevMessages, { user: 'AI', text: data.reply }]);
-    } catch (error) {
-      console.error('Error sending message:', error);
-    }
-  };
+  // const sendMessageToServer = async (message: string) => {
+  //   try {
+  //     setMessages(prevMessages => [...prevMessages, { user: 'You', text: message }]);
+  //     const response = await fetch('http://chat-app-load-balancer-381258406.us-east-1.elb.amazonaws.com/send-message', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({ message })
+  //     });
+  //     // const data = await response.json();
+  //     // Code to handle AI's response (currently commented out)
+  //     // setMessages(prevMessages => [...prevMessages, { user: 'AI', text: data.reply }]);
+  //   } catch (error) {
+  //     console.error('Error sending message:', error);
+  //   }
+  // };
 
-  useEffect(() => {
-    const eventSource = new EventSource('http://chat-app-load-balancer-381258406.us-east-1.elb.amazonaws.com/chat');
-    eventSource.onmessage = function(event) {
-      const newMessage: Message = JSON.parse(event.data);
+  // useEffect(() => {
+  //   const eventSource = new EventSource('http://chat-app-load-balancer-381258406.us-east-1.elb.amazonaws.com/chat');
+  //   eventSource.onmessage = function(event) {
+  //     const newMessage: Message = JSON.parse(event.data);
 
-      if (newMessage.text) {
-        const words = newMessage.text.split(' ');
-        let displayedMessage = "";
+  //     if (newMessage.text) {
+  //       const words = newMessage.text.split(' ');
+  //       let displayedMessage = "";
 
-        const renderMessage = () => {
-          if (words.length) {
-            displayedMessage += (displayedMessage ? ' ' : '') + words.shift();
-            setCurrentMessage(displayedMessage);
-            setTimeout(renderMessage, 200); // Adjust time as needed
-          } else {
-            setMessages(prevMessages => [...prevMessages, newMessage]);
-            setCurrentMessage("");
-          }
-        };
+  //       const renderMessage = () => {
+  //         if (words.length) {
+  //           displayedMessage += (displayedMessage ? ' ' : '') + words.shift();
+  //           setCurrentMessage(displayedMessage);
+  //           setTimeout(renderMessage, 200); // Adjust time as needed
+  //         } else {
+  //           setMessages(prevMessages => [...prevMessages, newMessage]);
+  //           setCurrentMessage("");
+  //         }
+  //       };
 
-        renderMessage();
-      }
-    };
+  //       renderMessage();
+  //     }
+  //   };
 
-    return () => {
-      eventSource.close();
-    };
-  }, []);
+  //   return () => {
+  //     eventSource.close();
+  //   };
+  // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <main
+          className={`flex h-screen w-screen flex-col text-sm text-white dark:text-white ${lightMode}`}
+        >
+         <div className="flex h-full w-full pt-[48px] sm:pt-0">
+      <div className="flex flex-1">
+      <Chat loading={true} apiKey={"test"}/>
+      </div>
+      </div>
+      </main>
+      {/* <header className="App-header">
         <h1>Chat Messages</h1>
         <div className="message-container">
           {messages.map((message, index) => (
@@ -83,7 +93,7 @@ function App() {
       <div>
         <input type="text" value={userInput} onChange={handleInputChange} />
         <button onClick={handleSendMessage}>Send</button>
-      </div>
+      </div> */}
     </div>
   );
 }
